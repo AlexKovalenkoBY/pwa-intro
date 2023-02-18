8818adcc3f0124c73bb4b18ea6edb9dece534d64
AlexKovalenkoBY <kovalenkoam@gmail.com>
2023-02-18 13:13:54 +0300

    added database

--- server/server.js
+++ server/server.js
@@ -1,1 +1,37 @@
 //backend 
+/* eslint-disable no-undef */
+'use strict'
+// var bodyParser = require('body-parser');
+const express = require('express')
+var sqlite3 = require('sqlite3');
+const server = express();
+const jsonParser = express.json();
+
+server.use(express.json({ limit: '50mb' }));
+server.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+
+// server.use(express.static(fullPath)) // ))__dirname + '/public'))
+server.use(express.static(__dirname))
+const compression = require('compression')
+
+
+// const db = require('../auto.db');
+const db = new sqlite3.Database('../auto.db');
+
+
+server.on("startup", function() {
+    console.log('About to exit.');
+    db.commit();
+
+});
+
+server.on('exit', function() {
+    console.log('About to exit.');
+    db.commit();
+
+});
+server.use(compression())
+server.listen(8888, function() {
+    console.log('Сервер ожидает подключения.on port 8888..' + new Date())
+})
